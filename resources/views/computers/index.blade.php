c4f1b00afd6b7fcb1f0a012b8ac4da01d27b03f4
Arnel Forbes <forbesarnel09@gmail.com>
2019-02-08 08:44:47 +0800

    clean navbar

--- resources/views/computers/index.blade.php
+++ resources/views/computers/index.blade.php
@@ -1,66 +1,62 @@
 @extends('shared.master')
 
-@section('title', 'Computers')
-
 @section('content')
+
 <div class="row justify-content-center">
-	<div class="col-sm-8">
-		
-		<div class="card my-3 border-secondary">
 
-			<div class="card-header">
-				
-			    <h2>Computer Inventory
-			    	@can ('create', App\Computer::class)
-					    <a class="btn btn-primary float-right" href="computers/create" role="button">Add New</a>
-			    	@endcan
-			    </h2>
-				
-			</div>
+<div class="col-sm-8">
 
-	    	<div class="card-body">
-			    
-			    @if (session('status'))
-				    <div class="alert alert-success">
-				        {{ session('status') }}
-				    </div>
-				@endif
+<div class="card my-3 border-secondary">
 
-			    <table class="table border-bottom">
-			    	<thead>
-			    		<tr class="text-center">
-			    			<th>CompName</th>
-			    			<th>Accounts</th>
-			    			<th>Status</th>
-			    			{{-- <th>Department</th> --}}
-			    		</tr>
-			    	</thead>
-			    	<tbody>
-			    		@foreach ($computers as $computer)
-			    			<tr class="text-center">
-			    				
-			    				<td>
-			    					<a href="computer/{{ $computer->id }}">
-				    					{{$computer->compName}}
-			    					</a>
-			    				</td>
-			    				<td>
-			    					@foreach ($computer->accounts as $account)
-				    					{{ $account['accountName'] }}
-			    						{{ !$loop->last ? '/ ' : '' }}
-			    					@endforeach
-			    				</td>
-			    				<td>{{$computer->status}}</td>
-			    				{{-- <td></td> --}}
+<div class="card-header">
 
-			    			</tr>
-			    		@endforeach
-			    	</tbody>
-			    </table>
-	    	</div>
-	    </div>
-	</div>
+<h2>Computer Inventory
+
+@can ('create', App\Computer::class)
+<a class="btn btn-primary float-right" href="computers/create" role="button">Add New</a>
+@endcan
+
+</h2>
+
+</div>
+
+<div class="card-body">
+
+@include('shared.status')
+
+<table class="table border-bottom">
+
+<thead>
+<tr class="text-center">
+<th>CompName</th>
+<th>Accounts</th>
+<th>Status</th>
+</tr>
+</thead>
+
+<tbody>
+@foreach ($computers as $computer)
+<tr class="text-center">
+<td>
+<a href="computer/{{ $computer->id }}">
+{{$computer->compName}}
+</a>
+</td>
+<td>
+@foreach ($computer->accounts as $account)
+{{ $account['accountName'] }}
+{{ !$loop->last ? '/ ' : '' }}
+@endforeach
+</td>
+<td>{{$computer->status}}</td>
+</tr>
+@endforeach
+</tbody>
+
+</table>
+</div>
+</div>
+</div>
 </div>
 
 @endsection
-
--- resources/views/shared/nav.blade.php
+++ resources/views/shared/nav.blade.php
@@ -1,62 +1,75 @@
 <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+
     <a class="navbar-brand" href="#">System Support</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+    <button class="navbar-toggler" type="button"
+        data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
         <ul class="navbar-nav ml-auto">
+
             <li class="nav-item">
                 <a class="nav-link" href="/">Home</a>
             </li>
+
             @auth
                 {{-- master-list --}}
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>Master List<span class="caret"></span>
+
+                    <a class="nav-link dropdown-toggle" href="#" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>Master List
+                        <span class="caret"></span>
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right">
-
                         <a class="dropdown-item" href="/staffs">Staff</a>
-
                         <a class="dropdown-item" href="/departments">Department</a>
-
                     </div>
+
                 </li>
                 {{-- computer --}}
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>Computer<span class="caret"></span>
+
+                    <a class="nav-link dropdown-toggle" href="#" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>Computer
+                        <span class="caret"></span>
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right">
-
                         <a class="dropdown-item" href="/computers">Computer</a>
-
                         <a class="dropdown-item" href="/softwares">Software</a>
-
                         <a class="dropdown-item" href="/softwares">Accessories</a>
-
                     </div>
+
                 </li>
                 {{-- document --}}
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>Document<span class="caret"></span>
+
+                    <a class="nav-link dropdown-toggle" href="#" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>Document
+                        <span class="caret"></span>
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right">
-
                         <a class="dropdown-item" href="/documents">Document</a>
-
                         <a class="dropdown-item" href="/document/categories">Category</a>
-
                     </div>
+
                 </li>
                 {{-- profile --}}
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{-- {{ Auth::user()->staff['firstName'] }} --}}
+
+                    <a class="nav-link dropdown-toggle" href="#" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         <img src="/storage/staffs/{{ Auth::user()->staff['image'] }}" alt=""
-                            style="height: 30px; width: 30px; border-radius: 15px;">
+                            style="
+                                height: 30px;
+                                width: 30px;
+                                border-radius: 15px;">
                         <span class="caret"></span>
                     </a>
 
@@ -66,7 +79,7 @@
                             href="/profile/{{ Auth::user()->staff['firstName'] . Auth::user()->staff['lastName'] }}">
                             {{ Auth::user()->staff['firstName'] . ' ' . Auth::user()->staff['lastName'] }}
                         </a>
-                        
+
                         @can ('isDeveloper', auth()->user())
                             <a class="dropdown-item" href="/dashboard" target="_blank">Dashboard</a>
                         @endcan
@@ -74,15 +87,17 @@
                         <div class="dropdown-divider"></div>
 
                         <a class="dropdown-item" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                             document.getElementById('logout-form').submit();">
+                            onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
                         </a>
 
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                             @csrf
                         </form>
-                    </div>
+
+                    </div>{{-- dropdowm-menu --}}
+
                 </li>
 
             @else
@@ -92,7 +107,9 @@
                 </li>
 
             @endauth
+
         </ul>
 
-    </div>
+    </div>{{-- collapse --}}
+
 </nav>
