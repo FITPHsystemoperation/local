269043e893644aac2eba69a94b7fc2a48330a1f1
Arnel Forbes <forbesarnel09@gmail.com>
2019-02-08 09:41:03 +0800

    clean computer create view

--- resources/views/computers/create.blade.php
+++ resources/views/computers/create.blade.php
@@ -1,65 +1,54 @@
 @extends('shared.master')
 
-@section('title', 'New Computer Record')
-
 @section('content')
-
 	<div class="row justify-content-center">
-	
 		<div class="col-sm-8">
-			
-		    <div class="card mt-3 border-secondary">
+			<div class="card mt-3 border-secondary">
+				<div class="card-header">
+					<h3>New Computer Record</h3>
+				</div>
 
-	    		<div class="card-header">
-	    			
-				    <h3>New Computer Record</h3>
-	    			
-	    		</div>
+				<div class="card-body">
+					@include('shared.error')
 
-		    	<div class="card-body">
+					<form method="post" action="/computers/create">
+						@csrf
 
-				    @foreach ($errors->all() as $error)
-					    <p class="alert alert-danger">{{ $error }}</p>
-					@endforeach
-		    		
-		    		<form method="post" action="/computers/create">
+						<fieldset class="form-group">
+							<label for="compName">Computer Name</label>
+							
+							<input type="text" class="form-control" id="compName" name="compName" placeholder="Computer Name"
+								value="{{ old('compName') }}" required autofocus>
+						</fieldset>
 
-		    			@csrf
+						<fieldset class="form-group">
+							<label for="os">Operating System</label>
+							
+							<input type="text" class="form-control" id="os" name="os" placeholder="Operating System"
+								value="{{ old('os') }}">
+						</fieldset>
 
-		    			<fieldset class="form-group">
-		    				<label for="compName">Computer Name</label>
-		    				<input type="text" class="form-control" id="compName" name="compName" placeholder="Computer Name" value="{{ old('compName') }}" required autofocus>
-		    			</fieldset>
+						<fieldset class="form-group">
+							<label for="status">Computer status</label>
+							
+							<input type="text" class="form-control" id="status" name="status" placeholder="Computer status"
+								value="{{ old('status') }}">
+						</fieldset>
 
-		    			<fieldset class="form-group">
-		    				<label for="os">Operating System</label>
-		    				<input type="text" class="form-control" id="os" name="os" placeholder="Operating System" value="{{ old('os') }}">
-		    			</fieldset>
+						<fieldset class="form-group">
+							<label for="information">Computer Information</label>
+							
+							<textarea class="form-control" id="information" name="information" rows="3">{{ old('information') }}</textarea>
+						</fieldset>
 
-		    			<fieldset class="form-group">
-		    				<label for="status">Computer status</label>
-		    				<input type="text" class="form-control" id="status" name="status" placeholder="Computer status" value="{{ old('status') }}">
-		    			</fieldset>
-
-		    			<fieldset class="form-group">
-		    				<label for="information">Computer Information</label>
-		    				<textarea class="form-control" id="information" name="information" rows="3">{{ old('information') }}</textarea>
-		    			</fieldset>
-		    			
 						<hr>
-		    		
-		    			<button type="submit" class="btn btn-primary">Save</button>
 
-		    			<a class="btn btn-outline-secondary" href="/computers" role="button">Back</a>
-		    		
-		    		</form>
+						<button type="submit" class="btn btn-primary">Save</button>
 
-		    	</div>
-
-		    </div>
-
-		</div>
-
-	</div>
-
+						<a class="btn btn-outline-secondary" href="/computers" role="button">Back</a>
+					</form>
+				</div>{{-- card-body --}}
+			</div>{{-- card --}}
+		</div>{{-- col --}}
+	</div>{{-- row --}}
 @endsection
