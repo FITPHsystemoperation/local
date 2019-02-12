145286f1fc9d289846e44cd1a829a9bbc6a5633b
Arnel Forbes <forbesarnel09@gmail.com>
2019-02-12 17:45:48 +0800

    clean document category show

--- resources/views/document/category/show.blade.php
+++ resources/views/document/category/show.blade.php
@@ -1,61 +1,53 @@
 @extends('shared.master')
 
 @section('content')
-<div class="row justify-content-center">
-	<div class="col-sm-8">
-		
-		<div class="card my-3 border-secondary">
-
+	<div class="container my-3">
+		<div class="card border-secondary">
 			<div class="card-header">
-				
-			    <h2>
-			    	{{ $category->categoryName }}
-			    	<a class="btn btn-outline-secondary float-right" href="/document/categories" role="button">Back</a>
-					<a href="/document/category/{{ $category->id }}/edit" class="btn btn-outline-info float-right mr-2">Edit</a>
-			    </h2>
-				
-			</div>
+				<div class="row">
+					<div class="col">
+						<h2>{{ $category->categoryName }}</h2>
+					</div>{{-- col --}}
+						
+					<div class="col text-right">
+						<a class="btn btn-outline-info" href="/document/category/{{ $category->id }}/edit">Update</a>
+						
+						<a class="btn btn-outline-secondary" href="/document/categories" role="button">Go Back</a>
+					</div>{{-- col --}}
+				</div>{{-- row --}}
+			</div>{{-- card-header --}}
 
-	    	<div class="card-body">
-			    
-			    @if (session('status'))
-				    <div class="alert alert-success">
-				        {{ session('status') }}
-				    </div>
-				@endif
+			<div class="card-body">
+				@include ('shared.status')
 
-			    <table class="table border-bottom">
-			    	<thead>
-			    		<tr class="text-center">
-			    			<th>Title</th>
-			    			<th>Uploaded Date</th>
-			    			<th>File</th>
-			    		</tr>
-			    	</thead>
-			    	<tbody>
-			    		@foreach ($category->documents as $document)
-			    			<tr class="text-center">
-			    				
-			    				<td>
-			    					<a href="/document/{{ $document->id }}">
-				    					{{$document->title}}
-			    					</a>
-			    				</td>
-			    				<td>{{ date('M d, Y', strtotime($document->files->last()->created_at)) }}</td>
-			    				<td>
-			    					<a href="/storage/documents/{{ $document->files->last()->filename }}" target="_blank">
-				    					{{ $document->files->last()->filename }}
-			    					</a>
-			    				</td>
-			    				
-			    			</tr>
-			    		@endforeach
-			    	</tbody>
-			    </table>
-	    	</div>
-	    </div>
-	</div>
-</div>
+				<table class="table border-bottom">
+					<thead>
+						<tr class="text-center">
+							<th>Title</th>
+							<th>Uploaded Date</th>
+							<th>File</th>
+						</tr>
+					</thead>
 
+					<tbody>
+						@foreach ($category->documents as $document)
+							<tr class="text-center">
+								<td>
+									<a href="/document/{{ $document->id }}">{{$document->title}}</a>
+								</td>
+
+								<td>{{ date('M d, Y', strtotime($document->files->last()->created_at)) }}</td>
+								
+								<td>
+									<a href="/storage/documents/{{ $document->files->last()->filename }}" target="_blank">
+										{{ $document->files->last()->filename }}
+									</a>
+								</td>
+							</tr>
+						@endforeach{{-- $category->documents as $document --}}
+					</tbody>
+				</table>
+			</div>{{-- card-body --}}
+		</div>{{-- card --}}
+	</div>{{-- container --}}
 @endsection
-
