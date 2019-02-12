41909747c1af0e4d55022a12291ab8f038dabae0
Arnel Forbes <forbesarnel09@gmail.com>
2019-02-12 08:37:45 +0800

    clean department edit

--- resources/views/departments/edit.blade.php
+++ resources/views/departments/edit.blade.php
@@ -1,48 +1,35 @@
 @extends('shared.master')
 
-@section('title', 'Update Department Record')
-
 @section('content')
+	<div class="container my-3">
+		<div class="card border-secondary">
+			<div class="card-header">
+				<h2>Update Department Record</h2>
+			</div>{{-- card-header --}}
 
-	<div class="row justify-content-center">
-	
-		<div class="col-sm-8">
-			
-		    <div class="card mt-3 border-secondary">
+			<div class="card-body">
+				@include ('shared.error') 
 
-		    	<div class="card-header">
-		    		
-				    <h2>Update Department Record</h2>
-		    		
-		    	</div>
+				<form method="post" action="/department/{{ $department->id }}/edit">
+					@csrf
 
-		    	<div class="card-body">
-				    
-				    @foreach ($errors->all() as $error)
-					    <p class="alert alert-danger">{{ $error }}</p>
-					@endforeach
-		    		
-		    		<form method="post" action="/department/{{ $department->id }}/edit">
+					<div class="form-group row">
+						<label for="departmentName" class="col-md-3 col-form-label text-md-right">Department Name</label>
+							
+						<div class="col-md-7">
+							<input type="text" class="form-control" id="departmentName" name="departmentName" placeholder="Department Name" value="{{ $department->departmentName }}" required autofocus>
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-		    			@csrf
+					<div class="form-group row mb-0">
+                        <div class="col-md-9 offset-md-3">
+							<button type="submit" class="btn btn-primary">update Record</button>
 
-		    			<fieldset class="form-group">
-		    				<label for="departmentName">Department Name</label>
-		    				<input type="text" class="form-control" id="departmentName" name="departmentName" placeholder="Department Name" value="{{ $department->departmentName }}" required autofocus>
-		    			</fieldset>
-
-		    			<button type="submit" class="btn btn-primary">Save</button>
-		    			
-		    			<a class="btn btn-outline-secondary" href="/department/{{ $department->id }}" role="button">Back</a>
-		    		
-		    		</form>
-
-		    	</div>
-
-		    </div>
-
-		</div>
-
-	</div>
-
+							<a class="btn btn-outline-secondary" href="/department/{{ $department->id }}" role="button">Go Back</a>
+                        </div>{{-- col --}}
+                    </div>{{-- row --}}
+				</form>
+			</div>{{-- card-body --}}
+		</div>{{-- card --}}
+	</div>{{-- container --}}
 @endsection
