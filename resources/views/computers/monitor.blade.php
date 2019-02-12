f4f53be012d99d804dfa0e7537cd6d0ff2c8b461
Arnel Forbes <forbesarnel09@gmail.com>
2019-02-12 15:40:11 +0800

    clean computer monitor

--- resources/views/computers/monitor.blade.php
+++ resources/views/computers/monitor.blade.php
@@ -1,90 +1,70 @@
 @extends('shared.master')
 
-@section('title', 'Add Monitor')
-
 @section('content')
+	<div class="container my-3">
+		<div class="card border-secondary">
+			<div class="card-header">
+				<h3>Select Existing Monitor</h3>
+			</div>{{-- card-header --}}
 
-	<div class="row justify-content-center">
-	
-		<div class="col-sm-8">
-			
-		    <div class="card mt-3 border-secondary">
+			<div class="card-body">
+				<form method="post" action="/computer/{{$id}}/monitor/add">
+					@csrf
 
-		    	<div class="card-header">
-		    		
-				    <h3>Select Existing Monitor</h3>
-		    		
-		    	</div>
+					<div class="form-group row">
+						<label for="monitor_id" class="col-md-3 col-form-label text-md-right">Select Monitor:</label>
+							
+						<div class="col-md-7">
+							<select class="c-select form-control" id="monitor_id" name="monitor_id" required autofocus>
+								@foreach ($monitors as $monitor)
+									<option value="{{ $monitor->id }}" {{ $monitor->computer_id ? 'disabled' : '' }}>
+										{{ $monitor->monitorName }}
+									</option>
+								@endforeach{{-- $monitors as $monitor --}}
+							</select>
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-		    	<div class="card-body">
+					<div class="form-group row mb-0">
+                        <div class="col-md-9 offset-md-3">
+							<button type="submit" class="btn btn-primary">Link Monitor</button>
 
-				    <form method="post" action="/computer/{{$id}}/monitor/add">
+							<a class="btn btn-outline-secondary" href="/computer/{{$id}}" role="button">Go Back</a>
+                        </div>{{-- col --}}
+                    </div>{{-- row --}}
+				</form>
+			</div>{{-- card-body --}}
+		</div>{{-- card --}}
 
-				    	@csrf
+		<div class="card mt-3 border-secondary">
+			<div class="card-header">
+				<h3>Add New Monitor</h3>
+			</div>{{-- card-header --}}
 
-				    	<fieldset class="form-group">
-				    		<label for="monitor_id">Select Monitor</label>
-				    		<select class="c-select form-control" id="monitor_id" name="monitor_id" required autofocus>
-				    			@foreach ($monitors as $monitor)
-				    				<option
-				    					value="{{ $monitor->id }}"
-				    					{{ $monitor->computer_id ? 'disabled' : '' }} 
-				    				>{{ $monitor->monitorName }}</option>
-				    			@endforeach
-				    		</select>
-				    	</fieldset>
+			<div class="card-body">
+				@include ('shared.error')
+				@include ('shared.status')
 
-			    		<button type="submit" class="btn btn-primary">Save</button>
+				<form method="post" action="/monitors/create">
+					@csrf
 
-			    		<a class="btn btn-outline-secondary" href="/computer/{{$id}}" role="button">Back</a>
+					<div class="form-group row">
+						<label for="monitorName" class="col-md-3 col-form-label text-md-right">Monitor Name:</label>
+							
+						<div class="col-md-7">
+							<input type="text" class="form-control" name="monitorName" placeholder="Monitor name" value="{{ old('monitorName') }}" required>
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-				    </form>
+					<div class="form-group row mb-0">
+                        <div class="col-md-9 offset-md-3">
+							<button type="submit" class="btn btn-primary">Save Monitor</button>
 
-		    	</div>
-
-		    </div>
-
-		    <div class="card mt-3 border-secondary">
-
-		    	<div class="card-header">
-		    		
-				    <h3>Add New Monitor</h3>
-		    		
-		    	</div>
-
-		    	<div class="card-body">
-
-				    @foreach ($errors->all() as $error)
-					    <p class="alert alert-danger">{{ $error }}</p>
-					@endforeach
-
-					@if (session('status'))
-					    <div class="alert alert-success">
-					        {{ session('status') }}
-					    </div>
-					@endif
-
-				    <form method="post" action="/monitors/create">
-
-				    	@csrf
-
-				    	<fieldset class="form-group">
-				    		<label for="monitorName">Monitor Name</label>
-				    		<input type="text" class="form-control" name="monitorName" placeholder="Monitor name" value="{{ old('monitorName') }}" required>
-				    	</fieldset>
-
-			    		<button type="submit" class="btn btn-primary">Add</button>
-
-			    		<button type="reset" class="btn btn-outline-secondary">Clear</button>
-
-				    </form>
-
-		    	</div>
-
-		    </div>
-
-		</div>
-
-	</div>
-
+							<button type="reset" class="btn btn-outline-secondary">Reset Form</button>
+                        </div>{{-- col --}}
+                    </div>{{-- row --}}
+				</form>
+			</div>{{-- card-body --}}
+		</div>{{-- card --}}
+	</div>{{-- container --}}
 @endsection
