b4e46c2da245e383f92d13004e408fb03749dcd1
Arnel Forbes <forbesarnel09@gmail.com>
2019-02-12 16:02:24 +0800

    clean software index

--- resources/views/softwares/index.blade.php
+++ resources/views/softwares/index.blade.php
@@ -1,59 +1,46 @@
 @extends('shared.master')
 
-@section('title', 'System Support')
-
 @section('content')
-	
-	<div class="row justify-content-center">
-		<div class="col-sm-8">
+	<div class="container my-3">
+		<div class="card border-secondary">
+			<div class="card-header">
+				<div class="row">
+					<div class="col">
+						<h2>Software Management</h2>
+					</div>{{-- col --}}
+						
+					<div class="col text-right">
+						<a class="btn btn-primary" href="/softwares/create" role="button">Add New</a>
+					</div>{{-- col --}}
+				</div>{{-- row --}}
+			</div>{{-- card-header --}}
 
-			<div class="card mt-3 border-secondary">
+			<div class="card-body">
+				@include ('shared.status')
 
-				<div class="card-header">
-					
-				    <h2>Software Management
-				    	<a class="btn btn-primary float-right" href="/softwares/create" role="button">Add New</a>
-				    </h2>
-				
-				</div>
+				<table class="table border-bottom">
+					<thead>
+						<tr class="text-center">
+							<th>Softwares</th>
+							<th>Action</th>
+						</tr>
+					</thead>
 
-		    	<div class="card-body">
-				    
-					@if (session('status'))
-					    <div class="alert alert-success">
-					        {{ session('status') }}
-					    </div>
-					@endif
-					
-				    <table class="table border-bottom">
-				    	<thead>
-				    		<tr class="text-center">
-				    			<th>Softwares</th>
-				    			<th>Action</th>
-				    		</tr>
-				    	</thead>
-				    	<tbody>
-				    		@foreach ($softwares as $software)
-
-				    			<tr class="text-center">
-				    				<td>
-				    					<a href="/software/{{ $software->id }}">
-					    					{{ $software->softwareName }}
-				    					</a>
-				    				</td>
-				    				<td>
-								    	<a class="btn btn-sm btn-outline-info" href="/software/{{ $software->id }}/edit" role="button">Edit</a>
-				    				</td>
-				    			</tr>
-
-				    		@endforeach
-				    	</tbody>
-				    </table>
-		    	</div>
-		    </div>	
-		</div>
-	</div>
-
-	
-
+					<tbody>
+						@foreach ($softwares as $software)
+							<tr class="text-center">
+								<td>
+									<a href="/software/{{ $software->id }}">{{ $software->softwareName }}</a>
+								</td>
+								
+								<td>
+									<a class="btn btn-sm btn-outline-info" href="/software/{{ $software->id }}/edit" role="button">Edit</a>
+								</td>
+							</tr>
+						@endforeach{{-- $softwares as $software --}}
+					</tbody>
+				</table>
+			</div>{{-- card-body --}}
+		</div>{{-- card --}}
+	</div>{{-- container --}}
 @endsection
