d3c05eca9cc2f7ac5f2b43f545f9749f0816ad0a
Arnel Forbes <forbesarnel09@gmail.com>
2019-02-12 16:37:43 +0800

    clean software show

--- resources/views/softwares/show.blade.php
+++ resources/views/softwares/show.blade.php
@@ -1,62 +1,49 @@
 @extends('shared.master')
 
-@section('title', 'System Support')
-
 @section('content')
-	
-	<div class="row justify-content-center">
-		<div class="col-sm-8">
+	<div class="container my-3">
+		<div class="card border-secondary">
+			<div class="card-header">
+				<div class="row">
+					<div class="col">
+						<h2>{{ ucwords($software->softwareName) }}</h2>
+					</div>{{-- col --}}
+						
+					<div class="col text-right">
+						<a class="btn btn-outline-secondary" href="/softwares" role="button">Go Back</a>
+					</div>{{-- col --}}
+				</div>{{-- row --}}
+			</div>{{-- card-header --}}
 
-			<div class="card mt-3 border-secondary">
+			<div class="card-body">
+				<table class="table border-bottom">
+					<thead>
+						<tr class="text-center">
+							<th>Computer</th>
+							
+							@foreach ($software->specList as $spec)
+								<th>{{ ucfirst($spec) }}</th>
+							@endforeach
+						</tr>
+					</thead>
 
-				<div class="card-header">
-					
-				    <h2>{{ $software->softwareName }}
-				    	<a class="btn btn-outline-secondary float-right" href="/softwares" role="button">Back</a>
-				    </h2>
-				
-				</div>
+					<tbody>
+						@foreach ($software->computers as $computer)
+							<tr class="text-center">
+								<td>
+									<a href="/computer/{{ $computer->computer->id }}">
+										{{ $computer->computer->compName }}
+									</a>
+								</td>
 
-		    	<div class="card-body">
-				    
-					@if (session('status'))
-					    <div class="alert alert-success">
-					        {{ session('status') }}
-					    </div>
-					@endif
-					
-				    <table class="table border-bottom">
-				    	<thead>
-				    		<tr class="text-center">
-				    			<th>Computer</th>
-				    			@foreach ($software->specList as $spec)
-				    				<th>{{ ucfirst($spec) }}</th>
-				    			@endforeach
-				    		</tr>
-				    	</thead>
-				    	<tbody>
-				    			@foreach ($software->computers as $computer)
-						    		<tr class="text-center">
-						    			<td>
-						    				<a href="/computer/{{ $computer->computer->id }}">
-							    				{{ $computer->computer->compName }}
-						    				</a>
-						    			</td>
-						    			@foreach ($software->specList as $spec)
-						    				<td>{{ 
-						    					array_key_exists($spec, $computer->specs) ?
-						    					$computer->specs[$spec] : '' }}
-						    				</td>
-						    			@endforeach
-						    		</tr>
-				    			@endforeach
-				    	</tbody>
-				    </table>
-		    	</div>
-		    </div>	
-		</div>
-	</div>
-
-	
-
+								@foreach ($software->specList as $spec)
+									<td>{{array_key_exists($spec, $computer->specs) ? $computer->specs[$spec] : '' }}</td>
+								@endforeach{{-- $software->specList as $spec --}}
+							</tr>
+						@endforeach{{-- $software->computers as $computer --}}
+					</tbody>
+				</table>
+			</div>{{-- card-body --}}
+		</div>{{-- card --}}
+	</div>{{-- container --}}
 @endsection
