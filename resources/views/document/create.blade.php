6af8769af353b2616f630e9826c79c35bda7fdfd
Arnel Forbes <forbesarnel09@gmail.com>
2019-02-12 17:01:58 +0800

    clean document create

--- resources/views/document/create.blade.php
+++ resources/views/document/create.blade.php
@@ -1,75 +1,69 @@
 @extends('shared.master')
 
 @section('content')
+	<div class="container my-3">
+		<div class="card border-secondary">
+			<div class="card-header">
+				<h2>New Document</h2>
+			</div>{{-- card-header --}}
 
-	<div class="row justify-content-center">
-	
-		<div class="col-sm-8">
-			
-		    <div class="card mt-3 border-secondary">
+			<div class="card-body">
+				@include ('shared.error')
 
-	    		<div class="card-header">
-				    <h2>New Document</h2>
-	    		</div>
+				<form method="post" action="/documents/create" enctype="multipart/form-data">
+					@csrf
 
-		    	<div class="card-body">
+					<div class="form-group row">
+						<label for="title" class="col-md-3 col-form-label text-md-right">Title:</label>
+							
+						<div class="col-md-7">
+							<input type="text" class="form-control" id="title" name="title" placeholder="Document Title" value="{{ old('title') }}" required autofocus>
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-				    @foreach ($errors->all() as $error)
-					    <p class="alert alert-danger">{{ $error }}</p>
-					@endforeach
-		    		
-		    		<form method="post" action="/documents/create" enctype="multipart/form-data">
+					<div class="form-group row">
+						<label for="file" class="col-md-3 col-form-label text-md-right">Attach Document:</label>
+						
+						<div class="col-md-7">
+							<input type="file" class="form-control-file" id="file" name="file" required>
 
-		    			@csrf
+							<small class="form-text text-muted">
+								<strong>Must not be greater than 10mb</strong>
+							</small>
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-		    			<fieldset class="form-group">
-		    				<label for="title">Title:</label>
-		    				<input type="text" class="form-control" id="title" name="title" placeholder="Document Title" value="{{ old('title') }}" required autofocus>
-		    			</fieldset>
+					<div class="form-group row">
+						<label for="category_id" class="col-md-3 col-form-label text-md-right">Category:</label>
+							
+						<div class="col-md-7">
+							<select class="form-control" id="category_id" name="category_id" required>
+								<option value="" disabled selected>Select Category</option>
+								
+								@foreach ($categories as $category)
+									<option value="{{ $category->id }}">{{ $category->categoryName }}</option>
+								@endforeach{{-- $categories as $category --}}
+							</select>
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-		    			<div class="row">
-		    				<div class="col-sm-2">
-			    				<label for="file">Attach Document:</label>
-		    				</div>
-		    				<div class="col-sm-5">
-			    				<input type="file" class="form-control-file" id="file" name="file" required>
-		    				</div>
-		    				<div class="col-sm-5">
-			    				<span class="text-info">*Must not be greater than 10mb.</span>
-		    				</div>
-		    			</div>
+					<div class="form-group row">
+						<label for="description" class="col-md-3 col-form-label text-md-right">Description:</label>
+							
+						<div class="col-md-7">
+							<textarea class="form-control" rows="3" id="description" name="description">{{ old('description') }}</textarea>
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-		    			<hr>
+					<div class="form-group row mb-0">
+                        <div class="col-md-9 offset-md-3">
+							<button type="submit" class="btn btn-primary">Save Record</button>
 
-		    			<fieldset class="form-group">
-		    				<label for="category_id">Category:</label>
-		    				<select class="form-control" id="category_id" name="category_id" required>
-		    					<option value="" disabled selected>Select Category</option>
-		    					@foreach ($categories as $category)
-		    						<option value="{{ $category->id }}">{{ $category->categoryName }}</option>
-		    					@endforeach
-		    				</select>
-		    			</fieldset>
-
-		    			<fieldset class="form-group">
-		    				<label for="description">Description:</label>
-		    				<textarea class="form-control" rows="3" id="description" name="description">{{ old('description') }}</textarea>
-		    			</fieldset>
-
-						<hr>
-		    		
-		    			<button type="submit" class="btn btn-primary">Save</button>
-
-		    			<a class="btn btn-outline-secondary" href="/documents" role="button">Back</a>
-		    			
-		    		</form>
-
-		    	</div>
-
-		    </div>
-
-		</div>
-
-	</div>
-
+							<a class="btn btn-outline-secondary" href="/documents" role="button">Go Back</a>
+                        </div>{{-- col --}}
+                    </div>{{-- row --}}
+				</form>
+			</div>{{-- card-body --}}
+		</div>{{-- card --}}
+	</div>{{-- container --}}
 @endsection
