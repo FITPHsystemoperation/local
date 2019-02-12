a93a84b0ff1603d0e0199fb934c7097d71b95ce8
Arnel Forbes <forbesarnel09@gmail.com>
2019-02-12 15:49:19 +0800

    clean computer charger

--- resources/views/computers/charger.blade.php
+++ resources/views/computers/charger.blade.php
@@ -1,90 +1,70 @@
 @extends('shared.master')
 
-@section('title', 'Add Charger')
-
 @section('content')
+	<div class="container">
+		<div class="card mt-3 border-secondary">
+			<div class="card-header">
+				<h3>Select Existing Charger</h3>
+			</div>{{-- card-header --}}
 
-	<div class="row justify-content-center">
-	
-		<div class="col-sm-8">
-			
-		    <div class="card mt-3 border-secondary">
+			<div class="card-body">
+				<form method="post" action="/computer/{{$id}}/charger/add">
+					@csrf
 
-		    	<div class="card-header">
-		    		
-				    <h3>Select Existing Charger</h3>
-		    		
-		    	</div>
+					<div class="form-group row">
+						<label for="charger_id" class="col-md-3 col-form-label text-md-right">Select Charger:</label>
+							
+						<div class="col-md-7">
+							<select class="c-select form-control" id="charger_id" name="charger_id" required autofocus>
+								@foreach ($chargers as $charger)
+									<option value="{{ $charger->id }}" {{ $charger->computer_id ? 'disabled' : '' }}>
+										{{ $charger->chargerName }}
+									</option>
+								@endforeach{{-- $chargers as $charger --}}
+							</select>
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-		    	<div class="card-body">
+					<div class="form-group row mb-0">
+                        <div class="col-md-9 offset-md-3">
+							<button type="submit" class="btn btn-primary">Link Charger</button>
 
-				    <form method="post" action="/computer/{{$id}}/charger/add">
+							<a class="btn btn-outline-secondary" href="/computer/{{$id}}" role="button">Go Back</a>
+                        </div>{{-- col --}}
+                    </div>{{-- row --}}
+				</form>
+			</div>{{-- card-body --}}
+		</div>{{-- card --}}
 
-				    	@csrf
+		<div class="card mt-3 border-secondary">
+			<div class="card-header">
+				<h3>Add New Charger</h3>
+			</div>{{-- card-header --}}
 
-				    	<fieldset class="form-group">
-				    		<label for="charger_id">Select Charger</label>
-				    		<select class="c-select form-control" id="charger_id" name="charger_id" required autofocus>
-				    			@foreach ($chargers as $charger)
-				    				<option
-				    					value="{{ $charger->id }}"
-				    					{{ $charger->computer_id ? 'disabled' : '' }} 
-				    				>{{ $charger->chargerName }}</option>
-				    			@endforeach
-				    		</select>
-				    	</fieldset>
+			<div class="card-body">
+				@include ('shared.error')
+				@include ('shared.status')
 
-			    		<button type="submit" class="btn btn-primary">Save</button>
+				<form method="post" action="/chargers/create">
+					@csrf
 
-			    		<a class="btn btn-outline-secondary" href="/computer/{{ $id }}" role="button">Back</a>
+					<div class="form-group row">
+						<label for="chargerName" class="col-md-3 col-form-label text-md-right">Charger Name:</label>
+							
+						<div class="col-md-7">
+							<input type="text" class="form-control" name="chargerName" placeholder="Charger name" value="{{ old('chargerName') }}" required>
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-				    </form>
+					<div class="form-group row mb-0">
+                        <div class="col-md-9 offset-md-3">
+							<button type="submit" class="btn btn-primary">Save Charger</button>
 
-		    	</div>
-
-		    </div>
-
-		    <div class="card mt-3 border-secondary">
-
-		    	<div class="card-header">
-		    		
-				    <h3>Add New Charger</h3>
-		    		
-		    	</div>
-
-		    	<div class="card-body">
-
-				    @foreach ($errors->all() as $error)
-					    <p class="alert alert-danger">{{ $error }}</p>
-					@endforeach
-
-					@if (session('status'))
-					    <div class="alert alert-success">
-					        {{ session('status') }}
-					    </div>
-					@endif
-
-				    <form method="post" action="/chargers/create">
-
-				    	@csrf
-
-				    	<fieldset class="form-group">
-				    		<label for="chargerName">Charger Name</label>
-				    		<input type="text" class="form-control" name="chargerName" placeholder="Charger name" value="{{ old('chargerName') }}" required>
-				    	</fieldset>
-
-			    		<button type="submit" class="btn btn-primary">Add</button>
-
-			    		<button type="reset" class="btn btn-outline-secondary">Clear</button>
-
-				    </form>
-
-		    	</div>
-
-		    </div>
-
-		</div>
-
-	</div>
-
+							<button type="reset" class="btn btn-outline-secondary">Reset Form</button>
+                        </div>{{-- col --}}
+                    </div>{{-- row --}}
+				</form>
+			</div>{{-- card-body --}}
+		</div>{{-- card --}}
+	</div>{{-- container --}}
 @endsection
