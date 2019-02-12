4ef26be386878a773bb7a5a5538fd849aa8f28b9
Arnel Forbes <forbesarnel09@gmail.com>
2019-02-12 17:51:39 +0800

    clean document category edit

--- resources/views/document/category/edit.blade.php
+++ resources/views/document/category/edit.blade.php
@@ -1,51 +1,44 @@
 @extends('shared.master')
 
 @section('content')
+	<div class="container my-3">
+		<div class="card mt-3 border-secondary">
+			<div class="card-header">
+				<h2>Update Category</h2>
+			</div>{{-- card-header --}}
 
-	<div class="row justify-content-center">
-	
-		<div class="col-sm-8">
-			
-		    <div class="card mt-3 border-secondary">
+			<div class="card-body">
+				@include ('shared.error')
 
-	    		<div class="card-header">
-				    <h2>Update Category</h2>
-	    		</div>
+				<form method="post" action="/document/category/{{ $category->id }}/edit">
 
-		    	<div class="card-body">
+					@csrf
 
-				    @foreach ($errors->all() as $error)
-					    <p class="alert alert-danger">{{ $error }}</p>
-					@endforeach
-		    		
-		    		<form method="post" action="/document/category/{{ $category->id }}/edit">
+					<div class="form-group row">
+						<label for="categoryName" class="col-md-3 col-form-label text-md-right">Category:</label>
+							
+						<div class="col-md-7">
+							<input type="text" class="form-control" id="categoryName" name="categoryName" placeholder="Category" value="{{ $category->categoryName }}" required autofocus>
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-		    			@csrf
+					<div class="form-group row">
+						<label for="description" class="col-md-3 col-form-label text-md-right">Description:</label>
+							
+						<div class="col-md-7">
+							<textarea class="form-control" rows="3" id="description" name="description">{{ $category->description }}</textarea>
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-		    			<fieldset class="form-group">
-		    				<label for="categoryName">Category:</label>
-		    				<input type="text" class="form-control" id="categoryName" name="categoryName" placeholder="Category" value="{{ $category->categoryName }}" required autofocus>
-		    			</fieldset>
+					<div class="form-group row mb-0">
+                        <div class="col-md-9 offset-md-3">
+							<button type="submit" class="btn btn-primary">Update Record</button>
 
-		    			<fieldset class="form-group">
-		    				<label for="description">Description:</label>
-		    				<textarea class="form-control" rows="3" id="description" name="description">{{ $category->description }}</textarea>
-		    			</fieldset>
-
-						<hr>
-		    		
-		    			<button type="submit" class="btn btn-primary">Update</button>
-
-		    			<a class="btn btn-outline-secondary" href="/document/categories" role="button">Back</a>
-		    			
-		    		</form>
-
-		    	</div>
-
-		    </div>
-
-		</div>
-
-	</div>
-
+							<a class="btn btn-outline-secondary" href="/document/category/{{ $category->id }}" role="button">Go Back</a>
+                        </div>{{-- col --}}
+                    </div>{{-- row --}}
+				</form>
+			</div>{{-- card-body --}}
+		</div>{{-- card --}}
+	</div>{{-- container --}}
 @endsection
