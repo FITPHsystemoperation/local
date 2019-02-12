2a51409c43930f917b84e86587b29447a864a24d
Arnel Forbes <forbesarnel09@gmail.com>
2019-02-12 08:53:32 +0800

    clean computer create

--- resources/views/computers/create.blade.php
+++ resources/views/computers/create.blade.php
@@ -1,54 +1,62 @@
 @extends('shared.master')
 
 @section('content')
-	<div class="row justify-content-center">
-		<div class="col-sm-8">
-			<div class="card mt-3 border-secondary">
-				<div class="card-header">
-					<h3>New Computer Record</h3>
-				</div>
+	<div class="container my-3">
+		<div class="card border-secondary">
+			<div class="card-header">
+				<h3>New Computer Record</h3>
+			</div>{{-- card-header --}}
 
-				<div class="card-body">
-					@include('shared.error')
+			<div class="card-body">
+				@include('shared.error')
 
-					<form method="post" action="/computers/create">
-						@csrf
+				<form method="post" action="/computers/create">
+					@csrf
 
-						<fieldset class="form-group">{{-- compName --}}
-							<label for="compName">Computer Name</label>
+					<div class="form-group row">{{-- compName --}}
+						<label for="compName" class="col-md-3 col-form-label text-md-right">Computer Name:</label>
 							
+						<div class="col-md-7">
 							<input type="text" class="form-control" id="compName" name="compName" placeholder="Computer Name"
-								value="{{ old('compName') }}" required autofocus>
-						</fieldset>
+							value="{{ old('compName') }}" required autofocus>
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-						<fieldset class="form-group">{{-- os --}}
-							<label for="os">Operating System</label>
+					<div class="form-group row">{{-- os --}}
+						<label for="os" class="col-md-3 col-form-label text-md-right">Operating System:</label>
 							
+						<div class="col-md-7">
 							<input type="text" class="form-control" id="os" name="os" placeholder="Operating System"
-								value="{{ old('os') }}">
-						</fieldset>
+							value="{{ old('os') }}">
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-						<fieldset class="form-group">{{-- status --}}
-							<label for="status">Computer status</label>
+					<div class="form-group row">{{-- status --}}
+						<label for="status" class="col-md-3 col-form-label text-md-right">Computer status:</label>
 							
+						<div class="col-md-7">
 							<input type="text" class="form-control" id="status" name="status" placeholder="Computer status"
-								value="{{ old('status') }}">
-						</fieldset>
+							value="{{ old('status') }}">
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-						<fieldset class="form-group">{{-- information --}}
-							<label for="information">Computer Information</label>
+					<div class="form-group row">{{-- information --}}
+						<label for="information" class="col-md-3 col-form-label text-md-right">Computer Information:</label>
 							
+						<div class="col-md-7">
 							<textarea class="form-control" id="information" name="information" rows="3">{{ old('information') }}</textarea>
-						</fieldset>
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-						<hr>
+					<div class="form-group row mb-0">
+                        <div class="col-md-9 offset-md-3">
+							<button type="submit" class="btn btn-primary">Save Record</button>
 
-						<button type="submit" class="btn btn-primary">Save</button>
-
-						<a class="btn btn-outline-secondary" href="/computers" role="button">Back</a>
-					</form>
-				</div>{{-- card-body --}}
-			</div>{{-- card --}}
-		</div>{{-- col --}}
-	</div>{{-- row --}}
+							<a class="btn btn-outline-secondary" href="/computers" role="button">Go Back</a>
+                        </div>{{-- col --}}
+                    </div>{{-- row --}}
+				</form>
+			</div>{{-- card-body --}}
+		</div>{{-- card --}}
+	</div>{{-- container --}}
 @endsection
