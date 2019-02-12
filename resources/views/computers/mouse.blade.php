34c8324fec179a901ea7715912abe1223ce6d6b9
Arnel Forbes <forbesarnel09@gmail.com>
2019-02-12 15:03:46 +0800

    clean computer mouse

--- resources/views/computers/mouse.blade.php
+++ resources/views/computers/mouse.blade.php
@@ -1,90 +1,70 @@
 @extends('shared.master')
 
-@section('title', 'Add Mouse')
-
 @section('content')
+	<div class="container my-3">
+		<div class="card border-secondary">
+			<div class="card-header">
+				<h3>Select Existing Mouse</h3>
+			</div>{{-- card-header --}}
 
-	<div class="row justify-content-center">
-	
-		<div class="col-sm-8">
-			
-		    <div class="card mt-3 border-secondary">
+			<div class="card-body">
+				<form method="post" action="/computer/{{$id}}/mouse/add">
+					@csrf
 
-		    	<div class="card-header">
-		    		
-				    <h3>Select Existing Mouse</h3>
-		    		
-		    	</div>
+					<div class="form-group row">
+						<label for="mouse_id" class="col-md-3 col-form-label text-md-right">Select Mouse:</label>
 
-		    	<div class="card-body">
+						<div class="col-md-7">
+							<select class="c-select form-control" id="mouse_id" name="mouse_id" required autofocus>
+								@foreach ($mouses as $mouse)
+									<option value="{{ $mouse->id }}" {{ $mouse->computer_id ? 'disabled' : '' }}>
+										{{ $mouse->mouseName }}
+									</option>
+								@endforeach{{-- $mouses as $mouse --}}
+							</select>
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-				    <form method="post" action="/computer/{{$id}}/mouse/add">
+					<div class="form-group row mb-0">
+                        <div class="col-md-9 offset-md-3">
+							<button type="submit" class="btn btn-primary">Link Mouse</button>
 
-				    	@csrf
+							<a class="btn btn-outline-secondary" href="/computer/{{$id}}" role="button">Go Back</a>
+                        </div>{{-- col --}}
+                    </div>{{-- row --}}
+				</form>
+			</div>{{-- card-body --}}
+		</div>{{-- card --}}
 
-				    	<fieldset class="form-group">
-				    		<label for="mouse_id">Select Mouse</label>
-				    		<select class="c-select form-control" id="mouse_id" name="mouse_id" required autofocus>
-				    			@foreach ($mouses as $mouse)
-				    				<option
-				    					value="{{ $mouse->id }}"
-				    					{{ $mouse->computer_id ? 'disabled' : '' }} 
-				    				>{{ $mouse->mouseName }}</option>
-				    			@endforeach
-				    		</select>
-				    	</fieldset>
+		<div class="card mt-3 border-secondary">
+			<div class="card-header">
+				<h3>Add New Mouse</h3>
+			</div>{{-- card-header --}}
 
-			    		<button type="submit" class="btn btn-primary">Save</button>
-			    		
-			    		<a class="btn btn-outline-secondary" href="/computer/{{$id}}" role="button">Back</a>
+			<div class="card-body">
+				@include ('shared.error')
+				@include ('shared.status')
 
-				    </form>
+				<form method="post" action="/mouses/create">
+					@csrf
 
-		    	</div>
+					<div class="form-group row">
+						<label for="mouseName" class="col-md-3 col-form-label text-md-right">Mouse Name:</label>
+							
+						<div class="col-md-7">
+							<input type="text" class="form-control" name="mouseName" placeholder="Mouse name" value="{{ old('mouseName') }}" required>
+						</div>{{-- col --}}
+					</div>{{-- row --}}
 
-		    </div>
+					<div class="form-group row mb-0">
+                        <div class="col-md-9 offset-md-3">
+							<button type="submit" class="btn btn-primary">Save Mouse</button>
 
-		    <div class="card mt-3 border-secondary">
-
-		    	<div class="card-header">
-		    		
-				    <h3>Add New Mouse</h3>
-		    		
-		    	</div>
-
-		    	<div class="card-body">
-
-				    @foreach ($errors->all() as $error)
-					    <p class="alert alert-danger">{{ $error }}</p>
-					@endforeach
-
-					@if (session('status'))
-					    <div class="alert alert-success">
-					        {{ session('status') }}
-					    </div>
-					@endif
-
-				    <form method="post" action="/mouses/create">
-
-				    	@csrf
-
-				    	<fieldset class="form-group">
-				    		<label for="mouseName">Mouse Name</label>
-				    		<input type="text" class="form-control" name="mouseName" placeholder="Mouse name" value="{{ old('mouseName') }}" required>
-				    	</fieldset>
-
-			    		<button type="submit" class="btn btn-primary">Add</button>
-
-			    		<button type="reset" class="btn btn-outline-secondary">Clear</button>
-
-				    </form>
-
-		    	</div>
-
-		    </div>
-
-		</div>
-
-	</div>
-
+							<button type="reset" class="btn btn-outline-secondary">Reset Form</button>
+                        </div>{{-- col --}}
+                    </div>{{-- row --}}
+				</form>
+			</div>{{-- card-body --}}
+		</div>{{-- card --}}
+	</div>{{-- container --}}
 @endsection
