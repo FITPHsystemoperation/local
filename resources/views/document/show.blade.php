e0c300e74de2689f8311ff7a0f6ac8c547931af2
Arnel Forbes <forbesarnel09@gmail.com>
2019-02-12 17:12:59 +0800

    clean document show

--- resources/views/document/show.blade.php
+++ resources/views/document/show.blade.php
@@ -1,31 +1,17 @@
 @extends('shared.master')
 
 @section('content')
-
-<div class="row justify-content-center">
-	<div class="col-sm-8">
-		
-		<div class="card my-3 border-secondary">
-
+	<div class="container my-3">
+		<div class="card border-secondary">
 			<div class="card-header">
-				
-			    <h2>{{ $document->title }}</h2>
-				
-			</div>
+				<h2>{{ $document->title }}</h2>
+			</div>{{-- card-header --}}
 
-	    	<div class="card-body">
-			    
-			    @if (session('status'))
-				    <div class="alert alert-success">
-				        {{ session('status') }}
-				    </div>
-				@endif
+			<div class="card-body">
+				@include ('shared.status')
+				@include ('shared.error')
 
-				@foreach ($errors->all() as $error)
-				    <p class="alert alert-danger">{{ $error }}</p>
-				@endforeach
-
-			   <h4 class="p-2">
+				<h4 class="p-2">
 					<span class="lead">Category:</span>
 					{{ $document->category->categoryName }}
 				</h4>
@@ -34,63 +20,61 @@
 					<span class="lead">Description:</span>
 					{{ $document->description }}
 				</h4>				    	
+			</div>{{-- card-body --}}
 
-	    	</div>
+			<div class="card-footer">
+				<a href="/document/{{ $document->id }}/edit" class="btn btn-info">Update</a>
 
-	    	<div class="card-footer">
-		    		
-				<a href="/document/{{ $document->id }}/edit" class="btn btn-info">Edit</a>
-				<a href="/documents" class="btn btn-outline-secondary">Back</a>
-	    	
-	    	</div>
-	    </div>
+				<a href="/documents" class="btn btn-outline-secondary">Go Back</a>
+			</div>{{-- card-footer --}}
+		</div>{{-- card --}}
 
-	    <div class="card mt-3 border-secondary">
+		<div class="card mt-3 border-secondary">
+			<div class="card-header">
+				<div class="row">
+					<div class="col">
+						<h4>Uploaded Files</h4>
+					</div>{{-- col --}}
+						
+					<div class="col text-right">
+						<button type="button" class="btn btn-primary" onclick="document.getElementById('file').click();">
+							Upload New
+						</button>
+					</div>{{-- col --}}
+				</div>{{-- row --}}
 
-		    	<div class="card-header">
-		    		
-				    <h4>
-				    	Files
-				    	<button type="button" class="btn btn-primary float-right"
-				    		onclick="document.getElementById('file').click();"
-				    		>Upload</button>
-				    </h4>
+				<form  method="post" action="/document/{{ $document->id }}/upload" enctype="multipart/form-data">
+					@csrf
+					<input type="file" id="file" name="file" style="display: none;" onchange="this.form.submit();"/>
+				</form>
+			</div>{{-- card-header --}}
 
-				    <form  method="post" action="/document/{{ $document->id }}/upload" enctype="multipart/form-data">
-				    	@csrf
-				    	<input type="file" id="file" name="file" style="display: none;" onchange="this.form.submit();"/>
-				    </form>
-		    		
-		    	</div>
+			<div class="card-body">
+				<table class="table border-bottom">
+					<thead>
+						<tr class="text-center">
+							<th>File Name</th>
+							<th>Date</th>
+							<th>Time</th>
+						</tr>
+					</thead>
 
-		    	<div class="card-body">
-
-					<table class="table border-bottom">
-						<thead>
+					<tbody>
+						@foreach ($document->files->reverse() as $file)
 							<tr class="text-center">
-								<th>File Name</th>
-								<th>Date</th>
-								<th>Time</th>
+								<td>
+									<a href="/storage/documents/{{ $file->filename }}" target="_blank">{{ $file->filename }}</a>
+								</td>
+
+								<td>{{ date('M d, Y', strtotime($file->created_at)) }}</td>
+								
+								<td>{{ date('h:i A', strtotime($file->created_at)) }}</td>
 							</tr>
-						</thead>
-						<tbody>
-							@foreach ($document->files->reverse() as $file)
-								<tr class="text-center">
-									<td>
-										<a href="/storage/documents/{{ $file->filename }}" target="_blank">{{ $file->filename }}</a>
-									</td>
-									<td>{{ date('M d, Y', strtotime($file->created_at)) }}</td>
-									<td>{{ date('h:i A', strtotime($file->created_at)) }}</td>
-								</tr>
-							@endforeach
-						</tbody>
-					</table>
-
-			    </div>
-
-		    </div>
-	</div>
-</div>
-
+						@endforeach{{-- $document->files->reverse() as $file --}}
+					</tbody>
+				</table>
+			</div>{{-- card-body --}}
+		</div>{{-- card --}}
+	</div>{{-- container --}}
 @endsection
 
