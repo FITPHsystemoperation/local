ad39d3f7317c39cc96d5a35a0cfc272b851880fc
Arnel Forbes <forbesarnel09@gmail.com>
2019-03-07 10:38:50 +0800

    document create

--- resources/views/document/create.blade.php
+++ resources/views/document/create.blade.php
@@ -33,11 +33,11 @@
 
 	    		    <div class="field">
 	    		    	<label class="label" for="file">Attach Document:</label>
-								    		    
+
 	    		        <div class="control">
-	    		            <div class="file has-name is-fullwidth">
+	    		            <div class="file has-name is-fullwidth {{ $errors->has('file') ? ' is-danger' : '' }}">
 	    		                <label class="file-label">
-	    		                    <input class="file-input" type="file" id="file" @change="selectFile($event)">
+	    		                    <input class="file-input" type="file" id="file" name="file" @change="selectFile($event)" required>
 	    		                    
 	    		                    <span class="file-cta">
 	    		                        <span class="file-icon">
@@ -54,6 +54,32 @@
 
 	    		        <p class="help is-danger">{{ $errors->first('file') }}</p>
 	    		    </div><!-- field -->
+
+	    		    <div class="field">
+	    		        <label class="label" for="category_id">Category:</label>
+	    		    
+	    		        <div class="control">
+	    		        	<div class="select is-fullwidth {{ $errors->has('title') ? ' is-danger' : '' }}">
+	    		        	    <select id="category_id" name="category_id" required>
+									<option value="" disabled selected>Select Category</option>
+									
+									@foreach ($categories as $category)
+										<option value="{{ $category->id }}">{{ $category->categoryName }}</option>
+									@endforeach{{-- $categories as $category --}}
+	    		        	    </select>
+	    		        	</div><!-- select -->
+	    		        </div><!-- control -->
+
+	    		        <p class="help is-danger">{{ $errors->first('category_id') }}</p>
+	    		    </div><!-- field -->
+					
+					<div class="field">
+					    <label class="label" for="description">Description:</label>
+					
+					    <div class="control">
+							<textarea id="description" name="description" rows="3" placeholder="Document description" class="textarea">{{ old('description') }}</textarea>
+					    </div><!-- control -->
+					</div><!-- field -->
 		        </section><!-- modal-card-body -->
 		        	
 		        <footer class="modal-card-foot">
@@ -64,69 +90,4 @@
 	        </form>
 	    </div><!-- modal-card -->
 	</div><!-- modal -->
-	<div class="container my-3">
-		<div class="card border-dark">
-			<div class="card-header">
-				<h2>New Document</h2>
-			</div>{{-- card-header --}}
-
-			<div class="card-body">
-				@include ('shared.error')
-
-				<form method="post" action="{{ route('documents.store') }}" enctype="multipart/form-data">
-					@csrf
-
-					<div class="form-group row">
-						<label for="title" class="col-md-3 col-form-label text-md-right">Title:</label>
-							
-						<div class="col-md-7">
-							<input type="text" class="form-control" id="title" name="title" placeholder="Document Title" value="{{ old('title') }}" required autofocus>
-						</div>{{-- col --}}
-					</div>{{-- row --}}
-
-					<div class="form-group row">
-						<label for="file" class="col-md-3 col-form-label text-md-right">Attach Document:</label>
-						
-						<div class="col-md-7">
-							<input type="file" class="form-control-file" id="file" name="file" required>
-
-							<small class="form-text text-muted">
-								<strong>Must not be greater than 10mb</strong>
-							</small>
-						</div>{{-- col --}}
-					</div>{{-- row --}}
-
-					<div class="form-group row">
-						<label for="category_id" class="col-md-3 col-form-label text-md-right">Category:</label>
-							
-						<div class="col-md-7">
-							<select class="form-control" id="category_id" name="category_id" required>
-								<option value="" disabled selected>Select Category</option>
-								
-								@foreach ($categories as $category)
-									<option value="{{ $category->id }}">{{ $category->categoryName }}</option>
-								@endforeach{{-- $categories as $category --}}
-							</select>
-						</div>{{-- col --}}
-					</div>{{-- row --}}
-
-					<div class="form-group row">
-						<label for="description" class="col-md-3 col-form-label text-md-right">Description:</label>
-							
-						<div class="col-md-7">
-							<textarea class="form-control" rows="3" id="description" name="description">{{ old('description') }}</textarea>
-						</div>{{-- col --}}
-					</div>{{-- row --}}
-
-					<div class="form-group row mb-0">
-                        <div class="col-md-9 offset-md-3">
-							<button type="submit" class="btn btn-primary">Save Record</button>
-
-							<a class="btn btn-outline-secondary" href="{{ route('documents.index') }}" role="button">Go Back</a>
-                        </div>{{-- col --}}
-                    </div>{{-- row --}}
-				</form>
-			</div>{{-- card-body --}}
-		</div>{{-- card --}}
-	</div>{{-- container --}}
 @endsection
