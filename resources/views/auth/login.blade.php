fb312a1aae1bb5849b742372ee40cdf288cd69ac
Arnel Forbes <forbesarnel09@gmail.com>
2019-03-15 19:19:57 +0800

    log in form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,63 +1,84 @@
-@extends('shared.master')
+@extends('shared.layout')
 
 @section('content')
-    <div class="container my-5">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card border-dark">
-                    <h5 class="card-header">{{ __('Login') }}</h5>
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        
+        <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">Login</p>
+                <a class="delete" href="{{ route('home') }}" aria-label="close"></a>
+            </header><!-- modal-card-head -->
+    
+            <form method="POST" @submit="submit" action="{{ route('login') }}">
+                @csrf
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
+                <section class="modal-card-body">
+                    <div class="field is-horizontal">
+                        <div class="field-label is-normal">
+                            <label class="label" for="idNumber">I.D. No.:</label>
+                        </div><!-- field-label -->
+                    
+                        <div class="field-body">
+                            <div class="field">
+                                <div class="control has-icons-right">
+                                    <input type="text" id="idNumber" name="idNumber" placeholder="I.D. Number."
+                                        class="input {{ $errors->has('idNumber') ? ' is-danger' : '' }}"
+                                        value="{{ old('idNumber') }}" required autofocus>
+                    
+                                    <span class="icon is-small is-right">
+                                        <i class="fas fa-user"></i>
+                                    </span><!-- icon -->
+                                </div><!-- control -->
+                    
+                                <p class="help is-danger">{{ $errors->first('idNumber') }}</p>
+                            </div><!-- field -->
+                        </div><!-- field-body -->
+                    </div><!-- field -->
 
-                            <div class="form-group row">{{-- idNumber --}}
-                                <label for="idNumber" class="col-md-4 col-form-label text-md-right">{{ __('I.D. Number') }}</label>
+                    <div class="field is-horizontal">
+                        <div class="field-label is-normal">
+                            <label class="label" for="password">Password:</label>
+                        </div><!-- field-label -->
+                    
+                        <div class="field-body">
+                            <div class="field">
+                                <div class="control has-icons-right">
+                                    <input type="password" id="password" name="password" placeholder="Password"
+                                        class="input {{ $errors->all() ? ' is-danger' : '' }}"
+                                        value="{{ old('password') }}" required>
+                    
+                                    <span class="icon is-small is-right">
+                                        <i class="fas fa-lock"></i>
+                                    </span><!-- icon -->
+                                </div><!-- control -->
+                    
+                                <p class="help is-danger">{{ $errors->first('password') }}</p>
+                            </div><!-- field -->
+                        </div><!-- field-body -->
+                    </div><!-- field -->
 
-                                <div class="col-md-6">
-                                    <input id="idNumber" type="text" class="form-control{{ $errors->has('idNumber') ? ' is-invalid' : '' }}" name="idNumber" value="{{ old('idNumber') }}" required autofocus>
+                    <div class="field is-horizontal">
+                        <div class="field-label is-normal"></div><!-- field-label -->
+                    
+                        <div class="field-body">
+                            <div class="field">
+                                <div class="control">
+                                    <label class="checkbox">
+                                        <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                    </label>
+                                </div><!-- control -->
+                            </div><!-- field -->
+                        </div><!-- field-body -->
+                    </div><!-- field -->
+                </section><!-- modal-card-body -->
+                
+                <footer class="modal-card-foot">
+                    <button type="submit" class="button is-primary" :class="{ 'is-loading': isLoading }">Log In</button>
 
-                                    @if ($errors->has('idNumber'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('idNumber') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group row">{{-- password --}}
-                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                    @if ($errors->has('password'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('password') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group row">{{-- remember --}}
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label class="form-check-label" for="remember">{{ __('Remember Me') }}</label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">{{ __('Login') }}</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>{{-- card-body --}}
-                </div>{{-- card --}}
-            </div>{{-- col --}}
-        </div>{{-- row --}}
-    </div>{{-- container --}}
+                    <my-link href="{{ route('home') }}">Go Home</my-link>
+                </footer><!-- modal-card-foot -->
+            </form>
+        </div><!-- modal-card -->
+    </div><!-- modal -->
 @endsection
