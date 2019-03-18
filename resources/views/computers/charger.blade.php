06fcc0ec3298537d106223b6d08bb09d07769ca6
Arnel Forbes <forbesarnel09@gmail.com>
2019-03-19 07:29:42 +0800

    computer charger ok

--- resources/views/computers/charger.blade.php
+++ resources/views/computers/charger.blade.php
@@ -11,8 +11,10 @@
 	        </header><!-- modal-card-head -->
 	
 	        <section class="modal-card-body">
+				@include ('shared.bulma-status')
+
 	        	<div class="box">
-					<form method="post" action="{{ route('computers.charger.attach', $computer->id) }}">
+					<form method="post" @submit="submit" action="{{ route('computers.charger.attach', $computer->id) }}">
 						@csrf
 						@method ('patch')
 
@@ -20,9 +22,8 @@
 						
 						<div class="field is-grouped">
 						    <div class="control is-expanded">
-								<div class="select is-fullwidth">
-								    <select id="charger" name="charger" autofocus>
-								    	<option></option>
+								<div class="select is-fullwidth {{ $errors->has('charger') ? 'is-danger': '' }}">
+								    <select id="charger" name="charger" autofocus required>
 										@foreach ($chargers as $charger)
 											<option value="{{ $charger->id }}" {{ $charger->computer ? 'disabled' : '' }}>
 												{{ $charger->chargerName }}
@@ -40,7 +41,32 @@
 					    <p class="help is-danger">{{ $errors->first('charger') }}</p>
 	        		</form>
 	        	</div>{{-- box --}}
-	            
+
+	        	<div class="box">
+					<form method="post" @submit="submit" action="{{ route('computers.charger.store', $computer->id) }}">
+						@csrf
+
+	        		    <label class="label" for="chargerName">Add New Charger:</label>
+		        		
+		        		<div class="field is-grouped">
+		        		    <div class="control is-expanded has-icons-right">
+		        		        <input type="text" id="chargerName" name="chargerName" placeholder="Charger Name"
+		        		            class="input {{ $errors->has('chargerName') ? ' is-danger' : '' }}"
+		        		            value="{{ old('chargerName') }}" required>
+		        		
+		        		        <span class="icon is-small is-right">
+		        		            <i class="fas fa-plug"></i>
+		        		        </span><!-- icon -->
+		        		    </div><!-- control -->
+
+		        		    <div class="field">
+		        		    	<button type="submit" class="button is-info" :class="{ 'is-loading': isLoading }">Record</button>
+		        		    </div>{{-- field --}}
+		        		</div><!-- field -->
+		        		
+	        		    <p class="help is-danger">{{ $errors->first('chargerName') }}</p>
+	        		</form>
+	        	</div>{{-- box --}}
 	        </section><!-- modal-card-body -->
 	        
 	        <footer class="modal-card-foot">
@@ -48,64 +74,4 @@
 	        </footer><!-- modal-card-foot -->
 	    </div><!-- modal-card -->
 	</div><!-- modal -->
-
-	<div class="container">
-		<div class="card mt-3 border-dark">
-			<div class="card-header">
-				<h3>Select Existing Charger</h3>
-			</div>{{-- card-header --}}
-
-			<div class="card-body">
-				@include ('shared.error')
-				@include ('shared.status')
-
-
-					<div class="form-group row">
-						<label for="charger" class="col-md-3 col-form-label text-md-right">Select Charger:</label>
-							
-						<div class="col-md-7">
-							<select class="c-select form-control" id="charger" name="charger" required autofocus>
-							</select>
-						</div>{{-- col --}}
-					</div>{{-- row --}}
-
-					<div class="form-group row mb-0">
-                        <div class="col-md-9 offset-md-3">
-							<button type="submit" class="btn btn-primary">Attach Charger</button>
-
-							<a class="btn btn-outline-secondary" role="button" href="{{ route('computers.show', $computer->id) }}">Go Back</a>
-                        </div>{{-- col --}}
-                    </div>{{-- row --}}
-				</form>
-			</div>{{-- card-body --}}
-		</div>{{-- card --}}
-
-		<div class="card mt-3 border-dark">
-			<div class="card-header">
-				<h3>Add New Charger</h3>
-			</div>{{-- card-header --}}
-
-			<div class="card-body">
-				<form method="post" action="{{ route('computers.charger.store', $computer->id) }}">
-					@csrf
-
-					<div class="form-group row">
-						<label for="chargerName" class="col-md-3 col-form-label text-md-right">Charger Name:</label>
-							
-						<div class="col-md-7">
-							<input type="text" class="form-control" name="chargerName" placeholder="Charger name" value="{{ old('chargerName') }}" required>
-						</div>{{-- col --}}
-					</div>{{-- row --}}
-
-					<div class="form-group row mb-0">
-                        <div class="col-md-9 offset-md-3">
-							<button type="submit" class="btn btn-primary">Save Charger</button>
-
-							<button type="reset" class="btn btn-outline-secondary">Reset Form</button>
-                        </div>{{-- col --}}
-                    </div>{{-- row --}}
-				</form>
-			</div>{{-- card-body --}}
-		</div>{{-- card --}}
-	</div>{{-- container --}}
 @endsection
