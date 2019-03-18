60f7f1e98f38491ec1ef687d5839f5ee151f16e0
Arnel Forbes <forbesarnel09@gmail.com>
2019-03-19 07:12:52 +0800

    computer charger

--- resources/views/computers/charger.blade.php
+++ resources/views/computers/charger.blade.php
@@ -1,6 +1,54 @@
-@extends('shared.master')
+@extends('shared.layout')
 
 @section('content')
+	<div class="modal is-active">
+	    <div class="modal-background"></div>
+	    
+	    <div class="modal-card">
+	        <header class="modal-card-head">
+	            <p class="modal-card-title">Charger</p>
+	            <a class="delete" href="{{ route('computers.show', $computer->id) }}" aria-label="close"></a>
+	        </header><!-- modal-card-head -->
+	
+	        <section class="modal-card-body">
+	        	<div class="box">
+					<form method="post" action="{{ route('computers.charger.attach', $computer->id) }}">
+						@csrf
+						@method ('patch')
+
+					    <label class="label" for="charger">Select Charger:</label>
+						
+						<div class="field is-grouped">
+						    <div class="control is-expanded">
+								<div class="select is-fullwidth">
+								    <select id="charger" name="charger" autofocus>
+								    	<option></option>
+										@foreach ($chargers as $charger)
+											<option value="{{ $charger->id }}" {{ $charger->computer ? 'disabled' : '' }}>
+												{{ $charger->chargerName }}
+											</option>
+										@endforeach{{-- $chargers as $charger --}}
+								    </select>
+								</div><!-- select -->
+						    </div><!-- control -->
+
+						    <div class="control">
+						    	<button type="submit" class="button is-link" :class="{ 'is-loading': isLoading }">Attach</button>
+						    </div>
+						</div><!-- field -->
+						
+					    <p class="help is-danger">{{ $errors->first('charger') }}</p>
+	        		</form>
+	        	</div>{{-- box --}}
+	            
+	        </section><!-- modal-card-body -->
+	        
+	        <footer class="modal-card-foot">
+	            <my-link href="{{ route('computers.show', $computer->id) }}">Go Back</my-link>
+	        </footer><!-- modal-card-foot -->
+	    </div><!-- modal-card -->
+	</div><!-- modal -->
+
 	<div class="container">
 		<div class="card mt-3 border-dark">
 			<div class="card-header">
@@ -11,21 +59,12 @@
 				@include ('shared.error')
 				@include ('shared.status')
 
-				<form method="post" action="{{ route('computers.charger.attach', $computer->id) }}">
-					@csrf
-
-					@method ('patch')
 
 					<div class="form-group row">
 						<label for="charger" class="col-md-3 col-form-label text-md-right">Select Charger:</label>
 							
 						<div class="col-md-7">
 							<select class="c-select form-control" id="charger" name="charger" required autofocus>
-								@foreach ($chargers as $charger)
-									<option value="{{ $charger->id }}" {{ $charger->computer ? 'disabled' : '' }}>
-										{{ $charger->chargerName }}
-									</option>
-								@endforeach{{-- $chargers as $charger --}}
 							</select>
 						</div>{{-- col --}}
 					</div>{{-- row --}}
