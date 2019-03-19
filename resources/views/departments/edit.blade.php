92a6ff6d781bb25f14bf087df2c551e76425ecec
Arnel Forbes <forbesarnel09@gmail.com>
2019-03-19 14:11:40 +0800

    department edit with bulma

--- resources/views/departments/edit.blade.php
+++ resources/views/departments/edit.blade.php
@@ -40,33 +40,4 @@
 		    </form>
 	    </div><!-- modal-card -->
 	</div><!-- modal -->
-	<div class="container my-3">
-		<div class="card border-dark">
-			<div class="card-header">
-				<h2>Update Department Record</h2>
-			</div>{{-- card-header --}}
-
-			<div class="card-body">
-				@include ('shared.error')
-
-
-					<div class="form-group row">
-						<label for="departmentName" class="col-md-3 col-form-label text-md-right">Department Name</label>
-							
-						<div class="col-md-7">
-							<input type="text" class="form-control" id="departmentName" name="departmentName" placeholder="Department Name" value="{{ $department->departmentName }}" required autofocus>
-						</div>{{-- col --}}
-					</div>{{-- row --}}
-
-					<div class="form-group row mb-0">
-                        <div class="col-md-9 offset-md-3">
-							<button type="submit" class="btn btn-primary">Update Record</button>
-
-							<a class="btn btn-outline-secondary" href="{{ route('departments.show', $department->id) }}" role="button">Go Back</a>
-                        </div>{{-- col --}}
-                    </div>{{-- row --}}
-				</form>
-			</div>{{-- card-body --}}
-		</div>{{-- card --}}
-	</div>{{-- container --}}
 @endsection
