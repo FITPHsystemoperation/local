ccb03d1384b8db8fa219daf6fc2d50af18239586
Arnel Forbes <forbesarnel09@gmail.com>
2019-03-20 12:07:26 +0800

    staff contact show with bulma

--- resources/views/staffs/partials/contact.blade.php
+++ resources/views/staffs/partials/contact.blade.php
@@ -1,39 +1,18 @@
-<div class="card mt-3 border-dark">{{-- contact-info --}}
-    <div class="card-header">
-        <div class="row">
-            <div class="col">
-                <h3>Contact Information</h3>
-            </div>{{-- col --}}
-                
-            <div class="col text-right">
-                <a href="{{ route('staffs.contact.edit', $staff->id) }}" class="btn btn-outline-info">Update</a>
-            </div>{{-- col --}}
-        </div>{{-- row --}}
-    </div>{{-- card-header --}}
+<article class="message">
+    <div class="message-header">
+        <p>Contact Information</p>
+        
+        <my-link class="is-warning is-rounded" lined="true" href="{{ route('staffs.contact.edit', $staff->id) }}" title="Update">
+            <span class="fa fa-edit"></span>
+        </my-link>
+    </div><!-- message-header -->
 
-    <div class="card-body">
-        <div class="row">
-            <div class="offset-md-1 col-sm-11">
-                <h4 class="p-2">
-                    <span class="lead">Contact No:</span>
-                    {{ $staff->contactNo }}
-                </h4>   
-
-                <h4 class="p-2">
-                    <span class="lead">Email Address:</span>
-                    {{ $staff->emailAddress }}
-                </h4>                   
-
-                <h4 class="p-2">
-                    <span class="lead">Permanent Address:</span>
-                    {{ $staff->permanentAddress }}
-                </h4>
-
-                <h4 class="p-2">
-                    <span class="lead">Present Address:</span>
-                    {{ $staff->presentAddress }}
-                </h4>
-            </div>{{-- col --}}
-        </div>{{-- row --}}
-    </div>{{-- card-body --}}
-</div>{{-- card --}}
+    <div class="message-body" style="padding-left: 5em;">
+        <div class="content">
+            <info attr="Contact No.">{{ $staff->contactNo }}</info>            
+            <info attr="Email Address">{{ $staff->emailAddress }}</info>            
+            <info attr="Permanent Address">{{ $staff->permanentAddress }}</info>            
+            <info attr="Present Address">{{ $staff->presentAddress }}</info>            
+        </div>{{-- content --}}
+    </div><!-- message-body -->
+</article><!-- message -->
