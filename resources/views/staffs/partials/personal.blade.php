431164c2ea3f34cf1c1f0e8a6102ec37ce149d12
Arnel Forbes <forbesarnel09@gmail.com>
2019-03-20 12:33:30 +0800

    staff personal show with bulma

--- resources/views/staffs/partials/personal.blade.php
+++ resources/views/staffs/partials/personal.blade.php
@@ -1,34 +1,17 @@
-<div class="card mt-3 border-dark">{{-- personal --}}
-    <div class="card-header">
-        <div class="row">
-            <div class="col">
-                <h3>Personal Information</h3>
-            </div>{{-- col --}}
-                
-            <div class="col text-right">
-                <a href="{{ route('staffs.personal.edit', $staff->id) }}" class="btn btn-outline-info">Update</a>
-            </div>{{-- col --}}
-        </div>{{-- row --}}
-    </div>
+<article class="message">
+    <div class="message-header">
+        <p>Personal Information</p>
 
-    <div class="card-body">
-        <div class="row">
-            <div class="offset-md-1 col-sm-11">
-                <h4 class="p-2">
-                    <span class="lead">Birth Date:</span>
-                    {{ date('F j, Y', strtotime($staff->birthday)) }}
-                </h4>   
+        <my-link class="is-warning is-rounded" lined="true" href="{{ route('staffs.personal.edit', $staff->id) }}" title="Update">
+            <span class="fa fa-edit"></span>
+        </my-link>
+    </div><!-- message-header -->
 
-                <h4 class="p-2">
-                    <span class="lead">Gender:</span>
-                    {{ $staff->gender === 'm' ? 'Male' : 'Female' }}
-                </h4>
-
-                <h4 class="p-2">
-                    <span class="lead">Civil Status:</span>
-                    {{ $staff->civilStatus }}
-                </h4>                   
-            </div>{{-- col --}}
-        </div>{{-- row --}}
-    </div>{{-- card-body --}}
-</div>{{-- card --}}
+    <div class="message-body" style="padding-left: 5em;">
+        <div class="content">
+            <info attr="Birth Date">{{ date('F j, Y', strtotime($staff->birthday)) }}</info>
+            <info attr="Gender">{{ $staff->gender === 'm' ? 'Male' : 'Female' }}</info>
+            <info attr="Civil Status">{{ $staff->civilStatus }}</info>
+        </div>{{-- content --}}
+    </div><!-- message-body -->
+</article><!-- message -->
--- resources/views/staffs/show.blade.php
+++ resources/views/staffs/show.blade.php
@@ -7,13 +7,9 @@
 
 			@if ($staff->isCompleted)
 				@include ('staffs.partials.work')
-
 				@include ('staffs.partials.contact')
-				
 				@include ('staffs.partials.emergency')
-
 				@include ('staffs.partials.account')
-				
 				@include ('staffs.partials.personal')
 			@endif{{-- ($staff->isCompleted) --}}
 	    </div><!-- container -->
