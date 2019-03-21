7657da995d5ef4f4348d9af8cc3e8af0b90e27c8
Arnel Forbes <forbesarnel09@gmail.com>
2019-03-21 12:50:05 +0800

    for optimization

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,13 +6,13 @@ use Illuminate\Support\Facades\Auth;
 
 class PagesController extends Controller
 {
-	public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function profile()
     {
     	return view('pages.profile')->with('staff', Auth::user()->staff);
+    }
+
+    public function home()
+    {
+        return view('pages.home');
     }
 }
